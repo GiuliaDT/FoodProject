d768c681e5f12c8b4875a3e52af255870d80487e
GiuliaDT <giuliaditomassi91@gmail.com>
2022-11-17 11:44:34 +0100

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">PlantyFood </Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>PlantyFood</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:slug" element={<Product />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product/:slug" element={<Product />} />
+            </Routes>
+          </Container>
         </main>
       </div>
     </BrowserRouter>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import data from '../data';
+import React, { useEffect, useReducer } from 'react';
+// import data from '../data';
+import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAILED':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function Home() {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: '',
+  });
+  // const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const res = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: res.data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAILED', payload: error.message });
+      }
+
+      //setProducts(res.data);
+    };
+    getData();
+  }, []);
   return (
     <div>
       <h1> Best Seller</h1>
       <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p> {product.name} </p>
-              </Link>
-              <p> {product.price}</p>
-              <button> Add to Cart</button>
-            </div>
-          </div>
-        ))}
+        <Row>
+          {products.map((product) => (
+            <Col sm={6} md={4} lg={3} className="mb-3">
+              <Product product={product}></Product>
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
